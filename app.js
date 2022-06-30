276ca7070dd403ef830acbdfba550d9cfe70600d
KrisAirdancer <chris.scott.marston@gmail.com>
2022-06-30 06:53:08 -0600

    Pulled code from the Node.js project over to give the project better structure.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-/***** IMPORTS *****/
+/* This file, the app.js file, is exactly the same as the server.js file we made earlier in this course
+ * (with maybe a few additional features). That is, they do the same thing, but this one takes advantage
+ * of the Express package for Node.js. The Express package simplifies
+ * Node.js development by abstracting away routing code and other common operations into functions built
+ * in Node.js. This simplifies our code and makes it faster and easier to write Node.js applications.
+ * 
+ * Note: The server.js file was deleted as part of this commit b/c it was replaced by the app.js file.
+ * To view the server.js file, go to the commit just before this one.
+ */
 
-// Imports the Express package
-require('dotenv/config');
 const express = require('express');
+const morgan = require('morgan'); // A middleware package for logging
 const mongoose = require('mongoose');
-const Post = require('./models/post'); // This pulls the Post Schema into the scope of this file.
+const postsRoutes = require('./routes/postsRoutes') // Importing the router from the blogRoutes.js file.
+require('dotenv/config');
 
-/***** SERVER SETUP *****/
-
-// Creates the server object as an express server.
-const app = express();
+// Setting up the Express app object
+const app = express(); // Initialize the Express app object. Not an instance of this file (app.js).
 
 // This is the connection string for MongoDB. It provides the login credentails and the information necessary to access the database over the internet.
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@goldenramenbowl.i1xlf.mongodb.net/goldenRamenBowl?retryWrites=true&w=majority`;
@@ -22,82 +28,43 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
     })
     .catch(err => { console.log(err.message) }); // TODO: Figure out a better way to deal with database connection issues. Is there a way to notify us if there is an issue here? At the very least, set this up to log to a logging file instead of the console so we can see the error logs after something happens.
 
-// EJS is a view engine. It allows you to write JavaScript directly in an html file (in this case a .ejs file). When that file is served to the browser, before it is sent, the view engine (EJS), and to some degree Express, will run that JavaScript and use it to generate additional HTML that it inserts into the HTML page before sending it to the browser. This is useful for inserting content from a database or filling the values of variables. Here's how to use it: https://www.youtube.com/watch?v=yXEesONd_54 and here's the docs https://ejs.co/
+
+// Register view engine (specify that this is the view engine this project is using - you should only ever use one)
 app.set('view engine', 'ejs');
+// EJS looks in the views folder for its files by default. This is changing that from the default "views" to "myviews"
+// app.set('views', "myviews"); // We aren't changing the directory for EJS files.
 
 // Allows app.js to use functions that can parse URL encoded data.
 app.use(express.urlencoded({ extended: true }));
 
-// This makes the directory 'public' and all of it's contents available to the frontend.
-app.use(express.static('public'));
+// Middleware & Static Files
+app.use(express.static('public')); // This makes the directory 'public' and all of it's contents available to the frontend.
 
-// This makes the directory 'public' and all of it's contents available to the frontend.
-app.use(express.static('public'));
+// Setting up logging with morgan. This loggs information to the console.
+app.use(morgan('dev'));
 
-/***** MONGODB COMMUNICATION *****/
+// End MongoDB database functions
 
-// This function creates a new post and adds it to the MongoDB database.
-app.get('/auto-post', (req, res) => {
-    // This creates a new instance of a Post object
-    const post = new Post({
-        title: 'Auto Post',
-        snippet: "An auto-generated post.",
-        body: "This post was auto-generated."
-    });
-
-    // This call saves the new post object to the MongoDB database. This is asynchronous and returns a Promise.
-    post.save()
-        .then( (result) => {
-            res.send(result); // This line sends the new post object (it's data) to the MongoDB database.
-        })
-        .catch( (err) => {
-            console.log(err.message);
-        })
-});
-
-// This function returns all of the posts saved in the MongoDB database.
-app.get('/get-all-posts', (req, res) => {
-    Post.find()
-        .then( (result) => {
-            res.send(result); // This sends the data (blog posts retrieved from the database) to the browser.
-        })
-        .catch( (err) => {
-            console.log(err.message);
-        })
-});
-
-app.get('/get-post', (req, res) => {
-    Post.findById('62bd02562df2e39ad99c7d36') // Mongoose handles the conversion of the Id from a string on our end to an ID object for use with MongoDB.
-        .then( (result) => {
-            res.send(result); // Sending the retrieved data to the browser
-        })
-        .catch( (err) => {
-            console.log(err.message);
-        })
-});
-
-/***** ROUTING *****/
-
-// This handles requests for the base domain https://www.goldenramenbowl.com/ - the homepage.
+// Listen for GET requests for the root of the domain ('/').
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home'} );
+    res.redirect('/posts'); // Redirects homepage traffic to the page displaying all of the blog posts.
 });
 
-/* 'app' is the server object. When a request is made to the server (browser sends a 
- *  request to the server), the server object (app) gets that request. We can access
- *  that request using .get, .post, etc. and respond to it. In this case, we parse the
- *  URL that comes in as a GET request and if it matches the specified path ('/about-us')
- *  we use the response object to send the desired HTML (.ejs) page back to the browser.
- */
+// /posts routes
+
+// The syntax for this can be .use(postsRoutes) or as below. If we use it as below, it only uses the routes in postsRoutes.js if the URL has /posts in it. As such, when we define the routes inside postsRoutes, we don't have to include /posts in front of every route in that file. Instead, in the postsRoutes.js file, we can just write the part of the route that comes after the /posts. eg. The route for /posts/:id would just be /:id in the postsRoutes.js file.
+app.use('/posts', postsRoutes); // This tells Node.js to apply all of the handlers that are defined in postsRoutes.js to the app.js object. This is equivalent to putting all of the code from postsRoutes.js right here. In fact, if you look at previous commits before Express was added, that is exactly what you will see.
+
+// Builds the about page.
 app.get('/about-us', (req, res) => {
     res.render('about-us', { title: 'About Us'} );
 });
 
-/*  Handling invalid URLs by redirecting that request to the 404 page. If no match is found in any of the above functions, this function will fire. If a match is found above, this function will never run.
- *  This function does not take in a URL as a parameter b/c it fires for any request. This is why this one must be at the bottom of the file. It is the default case if all others don't match the incoming request. If you move this up in the code, it will cause other valid URLs to go to the 404 page.
- *  This method does not automatically know that we're handling a 404 or serving any other kind of page for that matter. So we have to specify with the .status() function.
- *  This method is used to run middleware
- */
+// Handling invalid URLs by redirecting that request to the 404 page. If no match is found in any of the above functions, this function will fire. If a match is found above, this function will never run.
+// This function does not take in a URL as a parameter b/c it fires for any request. This is why this one must be at the bottom of the file. It is the default case if all others don't match the incoming request. If you move this up in the code, it will cause other valid URLs to go to the 404 page.
+// This method does not automatically know that we're handling a 404 or serving any other kind of page for that matter. So we have to specify with the .status() function.
+// This method is used to run middleware
 app.use( (req, res) => { 
     res.status(404).render('404', { title: '404'} );
 });
+
--- models/post.js
+++ models/post.js
@@ -1,8 +1,7 @@
-// This is a Schema for our posts. It defines the structure of the Documents (data) stored in the MongoDB database.
-// This Schema is then surrounded by a Model which provides an interface for communicating with the database for this Document type (Post type Document).
+// This file defines a model for a blog post.
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema; // This is a constructor function that we are using to create a Schema
+const Schema = mongoose.Schema; // .Schema is a constructor function
 
 // This defines a Schema for a blog post
 const postSchema = new Schema({
@@ -20,8 +19,8 @@ const postSchema = new Schema({
     }
 }, { timestamps: true }); // This will automatically genrate timestamps on each blog object
 
-// MongoDB & Mongoose will pluralize the name "Post" to "Posts" and that is the Collection it will store the Post objects in in the MongoDB database.
-const Post = mongoose.model('Post', postSchema); // This is the model for this Schema.
+// This will mean that mongoose is looking for the "Posts" collection in the MongoDB database. The name it looks for is the plural of the passed name.
+const Post = mongoose.model('Post', postSchema);
 
 // Exporting this module so that it can be used elsewhere in the project.
 module.exports = Post;
